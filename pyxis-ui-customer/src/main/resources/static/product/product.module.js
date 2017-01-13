73547c295374cf4565d455778d4c3c3aa3b168bc
mbmartinez <mbmartinez@exist.com>
2017-01-13 10:17:53 +0800

    Pyxis logo + favicon, product list initial

---
+++ pyxis-ui-customer/src/main/resources/static/product/product.module.js
@@ -0,0 +1,25 @@
+define([
+   'angular',
+   'landing/controller/ProductRootController',
+   'landing/controller/ProductListController'
+], function (angular, ProductRootController, ProductListController) {
+
+  console.debug('Configuring product.module');
+  angular.module('product.module', [])
+    .config(['$stateProvider', function ($stateProvider) {
+
+      $stateProvider.state('default.product', {
+        url: '/product',
+        template: '<ui-view></ui-view>',
+        controller: ProductRootController,
+        abstract: true
+      })
+      .state('default.product.list', {
+        url: '/list',
+        templateUrl: 'product/view/list.html',
+        controller: ProductListController
+      });
+
+    }]);
+
+});
