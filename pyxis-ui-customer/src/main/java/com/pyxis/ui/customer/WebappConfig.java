63cbc0ae81fe450b76482eee857df7794a7f7ab1
mbmartinez <mbmartinez@exist.com>
2017-01-15 22:55:55 +0800

    configure rest proxy controller for main api

--- pyxis-ui-customer/src/main/java/com/pyxis/ui/customer/WebappConfig.java
+++ pyxis-ui-customer/src/main/java/com/pyxis/ui/customer/WebappConfig.java
@@ -1,15 +1,27 @@
 package com.pyxis.ui.customer;
 
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import edu.wisc.my.restproxy.config.RestProxyConfiguration;
+import edu.wisc.my.restproxy.web.ResourceProxyController;
+
 @Configuration
+@Import(RestProxyConfiguration.class)
 public class WebappConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
       registry.addViewController("/").setViewName("index");
+    }
+
+    @Bean(autowire = Autowire.BY_TYPE)
+    public ResourceProxyController resourceProxyController() {
+        return new ResourceProxyController();
     }
 
 }
